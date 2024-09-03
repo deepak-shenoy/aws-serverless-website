4190111b3a229296d4078b8de7047eeaea306b01
Deepak Shenoy <shenoyd@jupiter.local>
2024-09-02 22:00:24 -0400

    Adding code for s3 bucket that will store content

--- lib/aws-serverless-website-stack.ts
+++ lib/aws-serverless-website-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class AwsServerlessWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsServerlessWebsiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const staticWebContentBucket = process.env["STATIC_WEB_CONTENT_BUCKET"]|| 'not-defined';
+    new s3.Bucket(this, staticWebContentBucket, {
+      bucketName: staticWebContentBucket,
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
   }
 }
