66cb64ac26ddec74d10bd968a951cc14343e982f
deepak-shenoy <shenoyd@jupiter.local>
2024-09-02 22:17:06 -0400

    Adding framework for cloudfront

--- lib/aws-serverless-website-stack.ts
+++ lib/aws-serverless-website-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 
 export class AwsServerlessWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,11 +10,17 @@ export class AwsServerlessWebsiteStack extends cdk.Stack {
     //
     // Bucket for static content
     //
-    const staticWebContentBucket = process.env["STATIC_WEB_CONTENT_BUCKET"]|| 'not-defined';
-    new s3.Bucket(this, staticWebContentBucket, {
-      bucketName: staticWebContentBucket,
+    const staticWebContentBucketName = process.env["STATIC_WEB_CONTENT_BUCKET"]|| 'not-defined';
+    const staticWebContentBucket = new s3.Bucket(this, staticWebContentBucketName, {
+      bucketName: staticWebContentBucketName,
       publicReadAccess: false,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
+
+    //
+    //
+    //
+    const oai = new cloudfront.OriginAccessIdentity(this, 'OAI');
+
   }
 }
