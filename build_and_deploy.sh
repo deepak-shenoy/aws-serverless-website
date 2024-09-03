42256f1b9a1d54175ba8fb9ced8983fa19ec5076
Deepak Shenoy <shenoyd@jupiter.local>
2024-09-02 22:01:23 -0400

    Adding code for s3 bucket that will store content

---
+++ build_and_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Build and Deploy
+# Deepak Shenoy
+# September 2nd, 2024
+#
+
+# Please setup environmental variables
+#     STATIC_WEB_CONTENT_BUCKET       - Name of bucket where web content will be stored
+
+# Called from IntelliJ
+cdk deploy
